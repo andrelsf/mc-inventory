101c06ebe7a05835fe5e3c4feb0fb9426d138c21
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 07:48:28 -0300

    Refactoring

--- src/main/java/br/dev/multicode/services/kafka/producers/InventoryProducer.java
+++ src/main/java/br/dev/multicode/services/kafka/producers/InventoryProducer.java
@@ -1,6 +1,8 @@
 package br.dev.multicode.services.kafka.producers;
 
-import br.dev.multicode.models.OrderInventoryStatus;
+import br.dev.multicode.models.OrderProcessingStatus;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import java.util.concurrent.CompletableFuture;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -15,11 +17,22 @@ public class InventoryProducer {
   private final Logger log = Logger.getLogger(this.getClass());
 
   @Inject
-  @Channel("sec-replay")
-  Emitter<OrderInventoryStatus> emitter;
+  @Channel("sec-response-status")
+  Emitter<OrderProcessingStatus> emitter;
 
-  public void sendToKafka(OrderInventoryStatus orderInventoryStatus)
+  public void doNotification(OrderProcessingStatus orderInventoryStatus)
   {
+    Uni.createFrom()
+        .item(orderInventoryStatus)
+        .emitOn(Infrastructure.getDefaultWorkerPool())
+        .subscribe()
+        .with(this::sendToKafka, Throwable::new);
+  }
+
+  private Uni<Void> sendToKafka(final OrderProcessingStatus orderInventoryStatus)
+  {
+    log.infof("Start of send message to Kafka topic sec-response-status");
+
     emitter.send(Message.of(orderInventoryStatus)
         .withAck(() -> {
           log.infof("Message sent successfully. eventId=%s", orderInventoryStatus.getEventId());
@@ -29,6 +42,8 @@ public class InventoryProducer {
           log.errorf("Message sent failed. ERROR: %s", throwable.getMessage());
           return CompletableFuture.completedFuture(null);
         }));
+
+    return Uni.createFrom().voidItem();
   }
 
 }
