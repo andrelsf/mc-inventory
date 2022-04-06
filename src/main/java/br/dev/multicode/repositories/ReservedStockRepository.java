7088c32b88c27e8a7b2cc40f1086c6d0ef6a001c
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 07:56:43 -0300

    Method save for reserved stock

---
+++ src/main/java/br/dev/multicode/repositories/ReservedStockRepository.java
@@ -0,0 +1,17 @@
+package br.dev.multicode.repositories;
+
+import br.dev.multicode.entities.ReservedStock;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import java.util.Set;
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class ReservedStockRepository implements PanacheRepository<ReservedStock> {
+
+  @Transactional
+  public void save(final Set<ReservedStock> reservedStocks)
+  {
+    this.persist(reservedStocks);
+  }
+}
