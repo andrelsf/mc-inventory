67a2165f4a9a12d5ef3a37ea549f07de830019b0
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 14:52:02 -0300

    Implementation processOrder method for inventory

--- src/main/java/br/dev/multicode/services/kafka/consumers/InventoryConsumer.java
+++ src/main/java/br/dev/multicode/services/kafka/consumers/InventoryConsumer.java
@@ -1,9 +1,11 @@
 package br.dev.multicode.services.kafka.consumers;
 
 import br.dev.multicode.models.OrderMessage;
+import br.dev.multicode.services.InventoryService;
 import io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata;
 import java.util.concurrent.CompletionStage;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.jboss.logging.Logger;
@@ -13,16 +15,20 @@ public class InventoryConsumer {
 
   private final Logger logger = Logger.getLogger(this.getClass());
 
+  @Inject
+  InventoryService inventoryService;
+
   @Incoming("sec-inventory")
   public CompletionStage<Void> receiveFromKafkaTopicSecInventory(Message<OrderMessage> orderMessage)
   {
     var metadata = orderMessage.getMetadata(IncomingKafkaRecordMetadata.class)
         .orElseThrow();
 
-    OrderMessage orderMessageReceived = orderMessage.getPayload();
+    final OrderMessage orderMessageReceived = orderMessage.getPayload();
     logger.infof("INVENTORY: %s - Got a order message: %s", metadata.getTopic(),
         orderMessageReceived.getOrderId());
 
+    inventoryService.processOrder(orderMessageReceived);
     return orderMessage.ack();
   }
 
