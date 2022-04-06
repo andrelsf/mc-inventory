d1b5f825e0c7664ad078ab098d468cf4ae139ad0
Andre Ferreira <andre.souza@sensedia.com>
2022-04-06 08:00:16 -0300

    Refactoring and added methods static for builder ReservedStock

--- src/main/java/br/dev/multicode/entities/ReservedStock.java
+++ src/main/java/br/dev/multicode/entities/ReservedStock.java
@@ -1,8 +1,14 @@
 package br.dev.multicode.entities;
 
+import br.dev.multicode.models.ItemMessage;
+import br.dev.multicode.models.OrderMessage;
 import java.time.ZonedDateTime;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -10,6 +16,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
@@ -22,6 +29,8 @@ public class ReservedStock {
 
   @Id
   @Column(name = "reserved_stock_id", length = 37, nullable = false)
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
   private String id;
 
   @Column(name = "order_id", length = 37, nullable = false)
@@ -39,4 +48,19 @@ public class ReservedStock {
   @UpdateTimestamp
   private ZonedDateTime updatedAt;
 
+  public static Set<ReservedStock> of(OrderMessage orderMessage) {
+    return orderMessage.getItems()
+        .stream()
+        .map(itemMessage -> ReservedStock.of(orderMessage.getOrderId(), itemMessage))
+        .collect(Collectors.toSet());
+  }
+
+  public static ReservedStock of(UUID orderId, ItemMessage itemMessage)
+  {
+    return ReservedStock.builder()
+        .orderId(orderId.toString())
+        .productId(itemMessage.getProductId())
+        .reservedQuantity(itemMessage.getAmount())
+        .build();
+  }
 }
